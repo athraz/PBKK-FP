123a3aca0f4d715f96b4f3a653c810c7e97ec771
athraz <athrazan2004@gmail.com>
2023-11-23 20:32:24 +0700

    feat: dashboard no longer need auth

--- routes/web.php
+++ routes/web.php
@@ -21,13 +21,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboard');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
+Route::get('/dashboard', function () {return view('dashboard');})->name('dashboard');
 
 Route::get('/profile', [ProfileController::class, 'edit'])->middleware(['auth'])->name('profile.edit');
 Route::patch('/profile', [ProfileController::class, 'update'])->middleware(['auth'])->name('profile.update');
