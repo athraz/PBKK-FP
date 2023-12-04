6c3e64146954cef3446262bb824013db53f96929
Sandhika21 <116061152+Sandhika21@users.noreply.github.com>
2023-12-04 15:48:30 +0700

    Update index.blade.php

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -66,7 +66,7 @@
                                         <p class="text-gray-950">Rp <span id="subtotal_<?= $cart['menu_id'] ?>">{{ number_format($cart->menu->price * $cart['quantity'], 2) }}</span></p>
                                     </div>
                                     <div class="w-1/6 px-4 flex items-center" style="height: 100%; overflow: hidden;">
-                                        <a href="/cart/{{ $cart->id }}" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
+                                        <a href="#" data-toggle="modal" data-target="#ModalDelete{{ $cart->id }}" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
                                             {{ __('Delete') }}
                                         </a>
                                     </div>
